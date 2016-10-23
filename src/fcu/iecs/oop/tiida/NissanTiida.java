41ac0ad2548f8e1bc94626ef329c772e124fa5c6
fcu-d0409874 <jim23693728@gmail.com>
2016-10-24 00:14:24 +0800

    修正

--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -3,13 +3,15 @@ package fcu.iecs.oop.tiida;
 public class NissanTiida {
 	public void tiida(int n) {
 		int a, i, j;
-		for (a = 0; a <= n; a++)
-			for (i = 1; i < n; i++) {
-				for (j = 1; j < a; j++) {
+		for (a = 1; a <= n; a++) { // LOOP幾次
+			for (i = 1; i <= a; i++) { // 幾行
+				for (j = 0; j < a; j++) { // 印幾個
 					System.out.print('*');
 				}
 				System.out.print("\n");
 			}
+			System.out.print("\n");
+		}
 	}
 
 }
